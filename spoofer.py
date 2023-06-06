c3b760558c50e6c6fec9573eb60daf8c87bcb6ef
ohadwolfman <ohad1696@gmail.com>
2023-06-06 20:00:11 +0300

    #4 commit

--- spoofer.py
+++ spoofer.py
@@ -1,3 +1,54 @@
+from scapy.all import *
+from scapy.layers.inet import IP, ICMP, UDP, TCP
+import random
 
-if __name__ == '__main__':
-    print('PyCharm')
+# a = IP()  # create an IP object
+# a.dst = '10.0.2.3'
+# b = ICMP()
+# p = a/b
+# send(p)
+
+# Function to spoof ICMP packets
+def spoof_icmp(target_ip, source_ip):
+    icmp = ICMP()
+    ip = IP(dst=target_ip, src=source_ip)
+    packet = ip / icmp
+    send(packet)
+
+# Function to spoof UDP packets
+def spoof_udp(target_ip, target_port, source_ip, source_port):
+    udp = UDP(dport=target_port, sport=source_port)
+    ip = IP(dst=target_ip, src=source_ip)
+    packet = ip / udp
+    send(packet)
+
+# Function to spoof TCP packets
+def spoof_tcp(target_ip, target_port, source_ip, source_port):
+    seq_num = random.randint(1000, 9000)
+    ack_num = 0
+    tcp = TCP(dport=target_port, sport=source_port, flags="S", seq=seq_num, ack=ack_num)
+    ip = IP(dst=target_ip, src=source_ip)
+    packet = ip / tcp
+    send(packet)
+
+
+# Specify the target IP and port
+target_ip = "192.168.0.100"  # Replace with the target IP address
+target_port = 1234  # Replace with the target port number
+
+# Specify the source IP and port for spoofing
+source_ip = "10.0.0.1"  # Replace with the desired source IP address
+source_port = 5678  # Replace with the desired source port number
+
+# Spoof ICMP packet
+spoof_icmp(target_ip, source_ip)
+
+# Spoof UDP packet
+spoof_udp(target_ip, target_port, source_ip, source_port)
+
+# Spoof TCP packet
+spoof_tcp(target_ip, target_port, source_ip, source_port)
+
+
+# if __name__ == '__main__':
+#     print('PyCharm')
--- traceroute.py
+++ traceroute.py
@@ -1,3 +1,40 @@
+from scapy.all import *
 
-if __name__ == '__main__':
-    print('PyCharm')
+def traceroute(destination):
+    ttl = 1
+    max_hops = 30  # Maximum number of hops to try
+
+    while True:
+        # Craft the packet with increasing TTL
+        packet = IP(dst=destination, ttl=ttl) / ICMP()
+
+        # Send the packet and receive the response
+        reply = sr1(packet, verbose=0, timeout=1)
+
+        if reply is None:
+            # No response received within the timeout
+            print(f"{ttl}. * * *")
+        elif reply.type == 0:
+            # Destination reached, ICMP Echo Reply received
+            print(f"{ttl}. {reply.src}")
+            break
+        else:
+            # Intermediate hop, ICMP Time Exceeded message received
+            print(f"{ttl}. {reply.src}")
+
+        ttl += 1
+
+        if ttl > max_hops:
+            # Maximum number of hops reached
+            print("Max hops reached.")
+            break
+
+
+# Specify the destination IP address or domain name
+destination = "www.example.com"  # Replace with your desired destination
+
+# Perform traceroute
+traceroute(destination)
+
+# if __name__ == '__main__':
+#     print('PyCharm')
